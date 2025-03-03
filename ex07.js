865eb2e404b635a9c0a5d37165fdd166fe8359f8
ZxineReal <stargolem90@gmail.com>
2025-03-03 10:34:30 +0700

    edit ex07

--- ex07.js
+++ ex07.js
@@ -1,6 +1,12 @@
 let number1 = prompt("Mời bạn nhập vào số thứ nhất: ");
 let number2 = prompt("Mời bạn nhập vào số thứ hai: ");
 let number3 = prompt("Mời bạn nhập vào số thứ ba: ");
-let biggestNumber = Math.max(number1, number2, number3);
+let biggestNumber = 0;
 
-console.log(`Số lớn nhất trong ba số là: ${biggestNumber}`);
+number1 > number2? 
+biggestNumber = number1
+: number2 > number3? 
+biggestNumber = number2
+: biggestNumber = number3;
+
+alert(`Số lớn nhất trong ba số là: ${biggestNumber}`);
